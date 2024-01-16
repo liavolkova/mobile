f60f70a839df0b55edbc815b632fada480fda6b7
Student <student@Mac.local>
2024-01-16 09:50:10 -0800

    First push - Hello World

---
+++ HelloWorld/HelloWorld/MainPage.xaml.cs
@@ -0,0 +1,25 @@
+﻿namespace HelloWorld;
+
+public partial class MainPage : ContentPage
+{
+
+	public MainPage()
+	{
+		InitializeComponent();
+	}
+
+	private void OnCounterClicked(object sender, EventArgs e)
+	{
+		if(lblDisplay.Text == "Hello World")
+		{
+			lblDisplay.Text = "Good day";
+		}
+		else
+		{
+			lblDisplay.Text = "Hello World";
+
+        }
+	}
+}
+
+
