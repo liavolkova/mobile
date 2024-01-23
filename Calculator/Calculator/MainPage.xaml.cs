b1bc44872bf2b988074d602f130059cc67047e48
Student <student@Mac.local>
2024-01-23 08:45:57 -0800

    Adding Add Master PRO

---
+++ Calculator/Calculator/MainPage.xaml.cs
@@ -0,0 +1,31 @@
+﻿namespace Calculator;
+
+public partial class MainPage : ContentPage
+{
+
+	public MainPage()
+	{
+		InitializeComponent();
+		Title = "Add Master Pro";
+	}
+
+    void Add_Clicked(System.Object sender, System.EventArgs e)
+    {
+        double dblFirst, dblSecond;
+       
+        Double.TryParse(txtFirstNum.Text, out dblFirst);
+        Double.TryParse(txtSecondNum.Text, out dblSecond);
+
+        lblDisplay.Text = (dblFirst + dblSecond).ToString();
+    }
+
+
+    void Clear_Clicked(System.Object sender, System.EventArgs e)
+    {
+        txtFirstNum.Text = string.Empty;
+        txtSecondNum.Text = string.Empty;
+        lblDisplay.Text = "0";
+    }
+}
+
+
